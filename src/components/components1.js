f145f376d3fa5116e8eedd88ef20b067eb631c86
Matichai <newxd123@gmail.com>
2023-11-18 23:49:25 +0700

    first commit

---
+++ src/components/components1.js
@@ -0,0 +1,13 @@
+function Profile() {
+    return (
+        <div className="content">
+            <img className="img1" src="./images/cpe.png" alt=""/>
+            <span className="CPE499">CPE499 <span className="ra">React</span></span>
+            <span className="name">6304837 Matichai Pomkaew </span>
+            <p className="eng">Computer Engineering</p>
+        </div>
+      
+    );
+  }
+  
+  export default Profile;
